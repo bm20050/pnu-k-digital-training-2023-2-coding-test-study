04b82fe596998880219e378870d48eacc85a584b
Bogol-E <skylark@skku.edu>
2023-04-06 13:51:57 +0900

    4/5

--- heejae/programers/176963.js
+++ heejae/programers/176963.js
@@ -2,7 +2,6 @@ function solution(name, yearning, photo) {
   for (let i of photo) i.filter(a => name.includes(a))
   return photo.map(i => i.reduce((a, b) => a + Number(yearning[name.indexOf(b)]), 0));
 }
-
 console.log(solution(
   ["may", "kein", "kain", "radi"], [5, 10, 1, 3],
   [["may", "kein", "kain", "radi"], ["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]
