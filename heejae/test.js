3453cb513da418fb488cb8045a63048b9ee93204
Bogol-E <skylark@skku.edu>
2023-04-09 06:16:12 +0900

    문제번호 다시 보기 힘들어 파일이름 수정하고 주석 담

--- heejae/test.js
+++ heejae/test.js
@@ -1,1 +1,14 @@
-console.log("U".toUpperCase())
+class abc {
+    name = 'kildong'
+    constructor (dkj){
+        this.name = dkj
+    }
+    
+    
+    a=()=>{
+        this.name = "haha";
+    }
+}
+let a= new abc('dsds')
+a.a();
+console.log(a)
