9fb52c9aa15b96ee7e7dd651c0eaccaaa6756d9a
Bogol-E <skylark@skku.edu>
2023-04-15 01:36:20 +0900

    34

--- heejae/test.js
+++ heejae/test.js
@@ -1,18 +1,19 @@
 function solution(tg) {
     function check(ary) {
-        ary = ary.sort((a, b) => b[1] - a[1]);
-        ary.sort((a, b) => a[0] - b[0]);
+        ary = ary.sort((a, b) => a[1] - b[1]);
+        ary.sort((a, b) => b[0] - a[0]);
+        let precur = 0;
         let cur = 0;
-        let ck = 0;
         let count = 1;
-        while (ck !== ary.length) {
-            if (ary[cur][1] <= ary[ck][0]){
-                cur=ck++;
+        while (cur !== ary.length) {
+            if (ary[precur][0] >= ary[cur][1]) {
+                precur = cur;
                 count++;
+                cur++;
             }
-            else ck++
+            else cur++
         }
-        return ary;
+        return count;
     }
     return check(tg);
 }
