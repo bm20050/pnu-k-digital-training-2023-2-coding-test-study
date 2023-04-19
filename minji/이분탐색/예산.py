e7af1030899cab20052f5c2e7a07662d24db7421
Erica-Ku <yeskuminji@naver.com>
2023-04-19 13:48:21 +0900

    Update 예산.py

--- minji/이분탐색/예산.py
+++ minji/이분탐색/예산.py
@@ -3,21 +3,5 @@ n = int(sys.stdin.readline().rstrip())
 array = list(map(int, sys.stdin.readline().split()))
 m = int(sys.stdin.readline().rstrip())
 
-start = min(array)
+start = 0
 end = max(array)
-m_list = []
-
-while start < end:
-    if sum(m_list) <= m:
-        mid = (start + end) // 2
-        for x in array:
-            if mid > x:
-                m_list.append(x)
-                end = mid - 1
-            else:
-                m_list.append(mid)
-                start = mid + 1
-    else:
-        break
-
-print(max(m_list))
