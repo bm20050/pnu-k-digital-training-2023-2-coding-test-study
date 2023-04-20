c6c648c763461311cbe506b95ce1c9e28c245043
Bogol-E <skylark@skku.edu>
2023-04-20 18:43:47 +0900

    89

--- heejae/solution.js
+++ heejae/solution.js
@@ -1,20 +1,30 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/132267
+// const [N, input] = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+// let ps=input.map(i=>i.split(' '));
+// ps = [[10, 5],[10,7],[1337,23],[123454321, 42],[999999937, 142857133]]
+ps = [[10, 7], [1337, 23], [123454321, 42],[999999937, 142857133]];
+let max = 10 ** 9;
+ps.forEach(i => {
+    let k = 1;
 
-// 내가 푼 꼬리재귀
-function solution(a, b, c, n = 0) {
-  if (a > c) return n;
-  return solution(a, b, c % a + ~~(c / a) * b, n + ~~(c / a) * b)
-}
+    while (k <= max) {
+        if ((i[0] * k +1 ) % (i[1]) === 0) {
+            console.log((i[0] * k +1 ) / (i[1]));
+            break;
+        }
+        else k++;
+    }
+    if (k == max) console.log('IMPOSSIBLE');
 
-// ??
-// https://blog.naver.com/doctorgu/222992407249 콜라병 문제 수학공식이 있는듯
-solution = (a, b, n) => Math.floor(Math.max(n - b, 0) / (a - b)) * b
+});
 
-
-
-console.log(solution(
-  2, 1, 20
-))
-// 19
-
+// 0:1만가능
+// 1:전부가능
+// 2:홀수만 가능
+// 3:전부가능
+// 4:홀수만 가능
+// 5:6,1만 가능
+// 6: 1,3,5,7,9만 가능
+// 7: 전부가능
+// 8: 홀수만 가능
+// 9: 전부가능
 
